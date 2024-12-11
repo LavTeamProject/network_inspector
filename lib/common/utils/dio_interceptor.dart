e0eefcd83d2176f6eff2718c4280283c761f46b6
LA <41138925+LavTeamProject@users.noreply.github.com>
2024-12-11 17:27:27 +0300

    Update dio_interceptor.dart

--- lib/common/utils/dio_interceptor.dart
+++ lib/common/utils/dio_interceptor.dart
@@ -68,12 +68,17 @@ class DioInterceptor extends Interceptor {
       if (isConsoleLogAllowed) {
         developer.log(logError);
       }
-      await saveResponse(err.response!);
-      await finishActivity(
-        err.response!,
-        err.response!.requestOptions.uri.toString(),
-        err.response!.data.toString(),
-      );
+      if (err != null && err.response != null) {
+        await saveResponse(err.response!);
+        await finishActivity(
+          err.response!,
+          err.response!.requestOptions.uri.toString(),
+          err.response!.data.toString(),
+        );
+      } else {
+        var payload = HttpResponse();
+        await networkInspector!.writeHttpResponseLog(payload);
+      }
     }
 
     var errorResponse = '\n[Error Response]'
